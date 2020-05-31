d18e29227d63bb3cad9724543be81fb9c895cb9d
apigott <aisling.pigott@colorado.edu>
2020-05-31 08:24:16 -0600

    plot most recent RL agg run

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 
 if __name__ == "__main__":
     a = Aggregator()
-    a.run()
+    # a.run()
 
     # plots using whatever datetime cycle is in the config file
     config_file = os.path.join("data", os.environ.get('CONFIG_FILE', 'config.json'))
@@ -18,8 +18,9 @@ if __name__ == "__main__":
     r = Reformat([os.path.join("outputs", "rl_agg", f"{start_dt.strftime('%Y-%m-%dT%H')}_{end_dt.strftime('%Y-%m-%dT%H')}-rl_agg_all-homes_20-horizon_8-results.json")])
 
     rlfile = os.path.join(r.outputs_dir, "rl_agg", f"{start_dt.strftime('%Y-%m-%dT%H')}_{end_dt.strftime('%Y-%m-%dT%H')}-rl_agg_all-homes_20-horizon_8-results.json")
+    rl_qfile = os.path.join(r.outputs_dir, "rl_agg", f"{start_dt.strftime('%Y-%m-%dT%H')}_{end_dt.strftime('%Y-%m-%dT%H')}-rl_agg_all-homes_20-horizon_8-iter-results.json")
     # adds the baseline if you've run the baseline, otherwise continues without plotting.
     mpc_noaggfile = os.path.join(r.outputs_dir, "baseline", f"{start_dt.strftime('%Y-%m-%dT%H')}_{end_dt.strftime('%Y-%m-%dT%H')}-baseline_all-homes_20-horizon_8-results.json")
     baselinefile = os.path.join(r.outputs_dir, "baseline", f"{start_dt.strftime('%Y-%m-%dT%H')}_{end_dt.strftime('%Y-%m-%dT%H')}-baseline_all-homes_20-horizon_1-results.json")
 
-    r.rl2baseline(rlfile, mpc_noaggfile, baselinefile)
+    r.rl2baseline(rlfile, rl_qfile, mpc_noaggfile, baselinefile)
