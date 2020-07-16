4e5ce6e957b8525f0337643bf82afdc2a2077d0c
apigott <aisling.pigott@colorado.edu>
2020-07-15 20:53:29 -0600

    latest

--- dragg/main.py
+++ dragg/main.py
@@ -7,24 +7,25 @@ import os
 import sys
 
 if __name__ == "__main__":
-    a = Aggregator("policy_update")
+    a = Aggregator()
     a.run()
 
-    agg_params = {"alpha": [0.001], "beta":[0.0], "epsilon":[0.32], "rl_horizon":[], "mpc_disutility":[]} # set parameters from earlier runs
-    mpc_params = {"mpc_hourly_steps": [4], "mpc_prediction_horizons": [1]}
-    date_ranges = {}
+    agg_params = {"alpha": [0.001, 0.005], "beta":[0.2], "epsilon":[0.1, 0.3], "rl_horizon":[], "mpc_disutility":[]} # set parameters from earlier runs
+    mpc_params = {"mpc_hourly_steps": [4], "mpc_prediction_horizons": [1], "mpc_discomfort":[3.0, 10.0]}
+    date_ranges = {"end_datetime": "2015-01-08 00"}
     # date_ranges = {}
     include_runs = {"baseline", "rl_agg"}
-    add_outputs = {"twin_q","polynomial_basis","policy_update"}
+    add_outputs = {}
     # add_outputs = {}
     r = Reformat(add_outputs=add_outputs, agg_params=agg_params, mpc_params=mpc_params, include_runs=include_runs, date_ranges=date_ranges)
-
+    # for i in r.parametrics:
+    #     print(i['results'])
     r.rl2baseline()
     r.rl2baseline_error()
     r.rl_thetas()
     r.rl_qvals()
     r.all_rps()
-    # r.plot_single_home2("Crystal-RXXFA") # pv_battery
+    r.plot_single_home2("Crystal-RXXFA") # pv_battery
     # if r.config["run_rl_agg"] or r.config["run_agg_mpc"] or r.config["run_rbo_mpc"]: # plots the home response if the actual community response is simulated
 
         # r.plo1t_single_home2(type="base")
