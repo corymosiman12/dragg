6e61df3eb0f5d6e2621129fc9236171b1f4dffd5
Aisling Pigott <aisling.pigott@colorado.edu>
2023-01-27 20:27:08 -0700

    add sysarg for redis and cleanup docker

---
+++ dragg/main.py
@@ -0,0 +1,15 @@
+from dragg.aggregator import Aggregator
+from dragg.reformat import Reformat
+import argparse
+
+REDIS_URL = 'redis://localhost'
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--redis", help="Redis host URL", default=REDIS_URL)
+
+    args = parser.parse_args()
+
+    a = Aggregator(redis_url=args.redis)
+    a.run()
